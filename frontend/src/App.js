8f3ed8609db17f7c75c32678c2303289da919538
jinish08 <jinishshah08@gmailcom>
2022-03-26 16:44:14 +0530

    landing page and explore page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Text } from '@chakra-ui/react';
+import React from "react";
+import { Text } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Home from "./pages/Explore";
 
 const App = () => {
-	return (
-		<Text fontSize="6xl" textAlign={'center'}>
-			App
-		</Text>
-	);
+  return (
+    <Routes>
+      <Route path="/landing" element={<Landing />} />
+	  <Route path="/">
+		  	<Route path="explore" element={<Home />} />
+	  </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/Landing.js
@@ -0,0 +1,33 @@
+import { Box, Button, Image, Text } from "@chakra-ui/react";
+import React from "react";
+// import bg from "./assets/landbg.svg"
+
+const Landing = () => {
+  return (
+    <Box>
+      <Image
+        zIndex="-1"
+        position="absolute"
+        src="/assets/landbg.svg"
+        height="100vh"
+        width="100vw"
+        objectFit="cover"
+        alt="Dan Abramov"
+        left="0"
+        top="0"
+      />
+      <Image src="assets/hero.svg" alt="hero" width="5xl" position="absolute"  zIndex="20" right="0" top="32"/>
+      <Box pl="32" pt="40">
+        <Text fontSize="6xl" fontWeight="bold" color="white" textAlign="left">
+            Feel the
+        </Text>
+        <Text fontSize="6xl" mt="-5" fontWeight="bold" color="white" textAlign="left">
+            music with us
+        </Text>
+        <Button mt="12" size="lg" px="8"  fontSize="2xl" colorScheme='brand' borderRadius="2xl" style={{boxShadow:"0px 4px 20px 8px #785fbb",border:"2px solid white"}}>Connect Now</Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Landing;
