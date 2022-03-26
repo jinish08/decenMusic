7357a12e4181ab96ba250e43eea6cc28b53921b8
Aadil Saudagar <aadilsaudagar244@gmail.com>
2022-03-26 22:36:32 +0530

    Buy Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import { Text } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Home from "./pages/Explore";
+import Info from "./pages/Info";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/landing" element={<Landing />} />
-	  <Route path="/">
-		  	<Route path="explore" element={<Home />} />
-	  </Route>
+      <Route path="/">
+        <Route path="explore" element={<Home />} />
+        <Route path="/buy/:id" element={<Info />} />
+      </Route>
     </Routes>
   );
 };
