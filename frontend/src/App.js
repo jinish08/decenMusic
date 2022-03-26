5f504ddfd90c9e16159b1ca5d0190085d3470fee
HetviSolanki49701 <hetvis.solanki@gmail.com>
2022-03-26 22:45:32 +0530

    made the form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Text } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Home from "./pages/Explore";
+import Form from "./pages/Form";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/landing" element={<Landing />} />
 	  <Route path="/">
 		  	<Route path="explore" element={<Home />} />
+		  	<Route path="form" element={<Form />} />
 	  </Route>
     </Routes>
   );
