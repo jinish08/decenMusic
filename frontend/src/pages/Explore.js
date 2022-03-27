70eff40038aa2075d81186e6a20e5756a741b2de
jinish08 <jinishshah08@gmailcom>
2022-03-27 08:16:50 +0530

    minor changes

--- frontend/src/pages/Explore.js
+++ frontend/src/pages/Explore.js
@@ -1,64 +1,118 @@
 import {
-	Box,
-	Text,
-	Center,
-	useColorModeValue,
-	Heading,
-	Stack,
-	Image,
-	HStack,
-	VStack,
-	SimpleGrid,
-	Flex,
-} from '@chakra-ui/react';
-import React, { useContext, useEffect } from 'react';
-import Card from '../components/Card';
-import SidebarWithHeader from '../components/SidebarWithHeader.tsx';
-import { UserContext } from '../providers/userContext';
+  Box,
+  Text,
+  Center,
+  useColorModeValue,
+  Heading,
+  Stack,
+  Image,
+  HStack,
+  VStack,
+  SimpleGrid,
+  Flex,
+  InputGroup,
+  InputLeftElement,
+  Input,
+} from "@chakra-ui/react";
+import React, { useContext, useEffect } from "react";
+import Card from "../components/Card";
+import SidebarWithHeader from "../components/SidebarWithHeader.tsx";
+import { UserContext } from "../providers/userContext";
+import {
+  collection,
+  doc,
+  updateDoc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+  increment,
+} from "firebase/firestore";
+import { db } from "../utils/firebase-config";
+import { FiSearch } from "react-icons/fi";
 
 const Explore = () => {
-	const { getAllNFTs, allNFTs } = useContext(UserContext);
+  const { getAllNFTs, allNFTs, currentAccount } = useContext(UserContext);
 
-	useEffect(() => {
-		getAllNFTs();
-	}, []);
+  useEffect(() => {
+    getAllNFTs();
+	getUser();
+  }, []);
 
-	return (
-		<>
-			<Image
-				zIndex="-1"
-				position="absolute"
-				src="/assets/explorebg.svg"
-				height="100vh"
-				width="100vw"
-				objectFit="cover"
-				alt="Dan Abramov"
-				left="0"
-				top="0"
-			/>
-			<Box position="absolute" width="full" zIndex="1">
-				<SidebarWithHeader />
-			</Box>
-			<Box postion="absolute" pl="16%" pt="20">
-				<VStack>
-					<Flex flexWrap={'wrap'} justifyContent={'space-evenly'}>
-						{allNFTs.length === 0 ? (
-							<Text fontSize={'4xl'}>No items</Text>
-						) : (
-							allNFTs?.map((i) => (
-								<Card
-									songSrc={i.song}
-									imageSrc={i.image}
-									seller={i.seller}
-									price={i.price}
-								/>
-							))
-						)}
-					</Flex>
-				</VStack>
-			</Box>
-		</>
-	);
+  console.log(allNFTs);
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  useEffect(()=>{
+	//   console.log(searchTerm)
+  },[searchTerm])
+
+
+  const getUser = async () => {
+	  try{
+		const userRef = collection(db, "user");
+		const q = query(userRef, where("address", "==", "abcdef"));
+		const querySnapshot = await getDocs(q);
+		const userID = querySnapshot.docs[0].id;
+		console.log(userID);
+		const userDoc = doc(db, "user", userID);
+		const docSnap = await getDoc(userDoc);
+		console.log(docSnap.data());
+	  } catch (error){
+		console.log(error);
+	  }
+  }
+
+  return (
+    <>
+      <Image
+        zIndex="-1"
+        position="absolute"
+        src="/assets/explorebg.svg"
+        height="100vh"
+        width="100vw"
+        objectFit="cover"
+        alt="Dan Abramov"
+        left="0"
+        top="0"
+      />
+      <Box position="absolute" width="full" zIndex="1">
+        <SidebarWithHeader searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+		
+      </Box>
+      <Box postion="absolute" pl="16%" pt="20">
+	  <InputGroup w={48} ml="36" zIndex={1}> 
+    <InputLeftElement
+      pointerEvents='none'
+      children={<FiSearch color='gray.300' />}
+    />
+    <Input type='tel' placeholder='search' onChange={(event)=>{setSearchTerm(event.target.value)}} />
+  </InputGroup>
+        <VStack>
+          <Flex flexWrap={"wrap"} justifyContent={"space-evenly"}>
+            {allNFTs.length === 0 ? (
+              <Text fontSize={"4xl"}>No items</Text>
+            ) : (
+				allNFTs?.filter((val)=>{
+					if(searchTerm==""){
+						return val
+					}
+					else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())){
+						return val
+					}
+				}).map((i) => (
+                <Card
+                  songSrc={i.song}
+                  imageSrc={i.image}
+                  seller={i.seller}
+                  price={i.price}
+                />
+              ))
+            )}
+          </Flex>
+        </VStack>
+      </Box>
+    </>
+  );
 };
 
 export default Explore;
