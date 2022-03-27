e755132074a3d70c8581950bf1522f829e4ce39b
Aadil Saudagar <aadilsaudagar244@gmail.com>
2022-03-27 10:54:50 +0530

    MyNFT Page & card changed

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -6,87 +6,96 @@ import {
 } from "react-icons/ai";
 
 import {
-	Box,
-	Text,
-	Center,
-	useColorModeValue,
-	Heading,
-	Stack,
-	Image,
-	Icon,
-	HStack,
-	Button,
-} from '@chakra-ui/react';
-import { AudioPlayer } from './AudioPlayer';
-import { FaEthereum } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
+  Box,
+  Text,
+  Center,
+  useColorModeValue,
+  Heading,
+  Stack,
+  Image,
+  Icon,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
+import { AudioPlayer } from "./AudioPlayer";
+import { FaEthereum } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 const IMAGE =
   "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80";
 
-const Card = ({ imageSrc, songSrc, price, name, sellerAddr, preview,tokenID }) => {
+const Card = ({
+  imageSrc,
+  songSrc,
+  price,
+  name,
+  sellerAddr,
+  preview,
+  tokenID,
+  sellingState,
+}) => {
   const [isLiked, setIsLiked] = useState(false);
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	return (
-		<>
-			<Box py={12}>
-				<Box
-					role={'group'}
-					p={6}
-					maxW={'330px'}
-					w={'full'}
-					bg={useColorModeValue('white', 'gray.800')}
-					boxShadow={'2xl'}
-					rounded={'lg'}
-					pos={'relative'}
-					zIndex={1}
-				>
-					<Box
-						rounded={'lg'}
-						mt={-12}
-						pos={'relative'}
-						height={'230px'}
-						_after={{
-							transition: 'all .3s ease',
-							content: '""',
-							w: 'full',
-							h: 'full',
-							pos: 'absolute',
-							top: 5,
-							left: 0,
-							backgroundImage: `url(${IMAGE})`,
-							filter: 'blur(15px)',
-							zIndex: -1,
-						}}
-						_groupHover={{
-							_after: {
-								filter: 'blur(20px)',
-							},
-						}}
-					>
-						<Image
-							rounded={'lg'}
-							height={220}
-							width={282}
-							objectFit={'cover'}
-							src={imageSrc}
-						/>
-					</Box>
-					<Stack
-						pt={10}
-						align={'center'}
-						alignItems="left"
-						justifyContent="flex-end"
-					>
-						<HStack>
-							<Heading
-								fontSize={'2xl'}
-								fontFamily={'body'}
-								fontWeight={800}
-								color={'#241432'}
-							>
-								{name}
-							</Heading>
+  return (
+    <>
+      <Box py={12}>
+        <Box
+          role={"group"}
+          p={6}
+          maxW={"330px"}
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          boxShadow={"2xl"}
+          rounded={"lg"}
+          pos={"relative"}
+          zIndex={1}
+        >
+          <Box
+            rounded={"lg"}
+            mt={-12}
+            pos={"relative"}
+            height={"230px"}
+            _after={{
+              transition: "all .3s ease",
+              content: '""',
+              w: "full",
+              h: "full",
+              pos: "absolute",
+              top: 5,
+              left: 0,
+              backgroundImage: `url(${IMAGE})`,
+              filter: "blur(15px)",
+              zIndex: -1,
+            }}
+            _groupHover={{
+              _after: {
+                filter: "blur(20px)",
+              },
+            }}
+          >
+            <Image
+              rounded={"lg"}
+              height={220}
+              width={282}
+              objectFit={"cover"}
+              src={imageSrc}
+            />
+          </Box>
+          <Stack
+            pt={10}
+            align={"center"}
+            alignItems="left"
+            justifyContent="flex-end"
+          >
+            <HStack>
+              <Heading
+                fontSize={"2xl"}
+                fontFamily={"body"}
+                fontWeight={800}
+                color={"#241432"}
+              >
+                {name}
+              </Heading>
 
               {isLiked ? (
                 <AiFillHeart
@@ -109,30 +118,33 @@ const Card = ({ imageSrc, songSrc, price, name, sellerAddr, preview,tokenID }) =
               {sellerAddr}
             </Text>
 
-						<Stack direction={'row'} align={'left'}>
-							<HStack>
-								<Text fontWeight={800} fontSize={'xl'} marginRight={120}>
-									<HStack>
-										<Text>{price}</Text>
-										<FaEthereum />
-									</HStack>
-								</Text>
-								<Button marginInlineEnd={'-14px'} onClick={(e) =>
-										navigate(`/buy/${tokenID}`, { state: { price: price } })
-									}>
-									Buy Now
-									<AiOutlineShoppingCart />
-								</Button>
-							</HStack>
-						</Stack>
-						<Box w={10}>
-							{songSrc && <AudioPlayer preview={preview} songSrc={songSrc} />}
-						</Box>
-					</Stack>
-				</Box>
-			</Box>
-		</>
-	);
+            <Stack direction={"row"} align={"left"}>
+              <HStack>
+                <Text fontWeight={800} fontSize={"xl"} marginRight={120}>
+                  <HStack>
+                    <Text>{price}</Text>
+                    <FaEthereum />
+                  </HStack>
+                </Text>
+                <Button
+                  marginInlineEnd={"-14px"}
+                  onClick={(e) =>
+                    navigate(`/buy/${tokenID}`, { state: { price: price } })
+                  }
+                >
+                  {sellingState ? "Sell Now" : "Buy Now"}
+                  <AiOutlineShoppingCart />
+                </Button>
+              </HStack>
+            </Stack>
+            <Box w={10}>
+              {songSrc && <AudioPlayer preview={preview} songSrc={songSrc} />}
+            </Box>
+          </Stack>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Card;
--- frontend/src/pages/MyNft.js
+++ frontend/src/pages/MyNft.js
@@ -3,40 +3,56 @@ import React, { useContext, useEffect } from "react";
 import Card from "../components/Card";
 import SidebarWithHeader from "../components/SidebarWithHeader.tsx";
 import { UserContext } from "../providers/userContext";
-import { VStack, Flex, Box } from "@chakra-ui/react";
+import { VStack, Flex, Box, Image } from "@chakra-ui/react";
 
 const MyNft = () => {
   const { getMyNFTs, myNfts } = useContext(UserContext);
   useEffect(() => {
     getMyNFTs();
   }, []);
+
   return (
-    <div>
-      <SidebarWithHeader />
-      {/* <Box postion="absolute" pl="16%" pt="20" top="10" left="0">
+    <>
+      <Image
+        zIndex="-1"
+        position="absolute"
+        src="/assets/explorebg.svg"
+        height="100vh"
+        width="100vw"
+        objectFit="cover"
+        alt="Dan Abramov"
+        left="0"
+        top="0"
+      />
+      <div>
+        <SidebarWithHeader />
+        {/* <Box postion="absolute" pl="16%" pt="20" top="10" left="0">
         <VStack> */}
-      {myNfts.length === 0 ? (
-        <Text fontSize={"4xl"}>No items</Text>
-      ) : (
-        <div style={{ position: "absolute", top: "150px", left: "280px" }}>
-          <Flex flexWrap={"wrap"} justifyContent="space-evenly">
-            {myNfts?.map((i) => (
-              <div style={{ marginRight: "20px" }}>
-                <Card
-                  songSrc={i.song}
-                  imageSrc={i.image}
-                  seller={i.seller}
-                  price={i.price}
-                  tokenID={i.tokenId}
-                />
-              </div>
-            ))}
-          </Flex>
-        </div>
-      )}
-      {/* </VStack>
+        {myNfts.length === 0 ? (
+          <Text fontSize={"4xl"}>No items</Text>
+        ) : (
+          <div style={{ position: "absolute", top: "150px", left: "280px" }}>
+            <Flex flexWrap={"wrap"} justifyContent="space-evenly">
+              {myNfts?.map((i) => (
+                <div style={{ marginRight: "20px" }}>
+                  <Card
+                    songSrc={i.song}
+                    imageSrc={i.image}
+                    seller={i.seller}
+                    price={i.price}
+                    tokenID={i.tokenId}
+                    preview={false}
+                    sellingState={true}
+                  />
+                </div>
+              ))}
+            </Flex>
+          </div>
+        )}
+        {/* </VStack>
       </Box> */}
-    </div>
+      </div>
+    </>
   );
 };
 
